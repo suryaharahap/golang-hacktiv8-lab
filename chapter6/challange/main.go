9a08b2989d572eb6cfb1115bb534780cbb1e1504
suryaharahap <suryaharahap18@gmail.com>
2024-06-02 23:26:46 +0700

    fix: improve code challange 6

--- chapter6/challange/main.go
+++ chapter6/challange/main.go
@@ -10,27 +10,17 @@ import (
 )
 
 func main() {
-	// Initialize database connection
-	// dbConnection, err := db.NewPostgresDB()
-	// if err != nil {
-	// 	log.Fatalf("failed to connect to database: %v", err)
-	// }
-
-	// dbConnection, err := db.NewPostgresDB()
 	dbConnection, err := database.NewPostgresDB()
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 
-	// Initialize repositories, services and handlers
 	productRepo := product.NewGormProductRepository(dbConnection)
 	productService := services.NewProductService(productRepo)
 	productHandler := api.NewProductHandler(productService)
 
-	// Initialize router
 	r := router.InitRouter(productHandler)
 
-	// Start server
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to start server: %v", err)
 	}
