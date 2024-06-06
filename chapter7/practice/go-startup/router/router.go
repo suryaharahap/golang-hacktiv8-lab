80f01baf699ba94dac591996e7b7a0a02cb88128
suryaharahap <suryaharahap18@gmail.com>
2024-06-06 17:56:00 +0700

    feat: add upload avatar profile images endpoint

--- chapter7/practice/go-startup/router/router.go
+++ chapter7/practice/go-startup/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"fmt"
 	"golang-hactiv8-lab/chapter7/practice/go-startup/helper"
 	"golang-hactiv8-lab/chapter7/practice/go-startup/models"
 	"net/http"
@@ -110,6 +111,45 @@ func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
 	}
 
 	response := helper.APIResponse(metaMessage, http.StatusOK, "success", data)
+	c.JSON(http.StatusOK, response)
+
+}
+
+func (h *userHandler) UploadAvatar(c *gin.Context) {
+	file, err := c.FormFile("avatar")
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	userID := 1
+
+	// path := "images/" + file.Filename
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	_, err = h.userService.SaveAvatar(userID, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{"is_uploaded": false}
+	response := helper.APIResponse("Avatar successfuly uploaded", http.StatusBadRequest, "success", data)
+
 	c.JSON(http.StatusOK, response)
 
 }
