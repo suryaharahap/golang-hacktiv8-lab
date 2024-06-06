9b4bdfeded426cc8c791717f4cfb649a0279ad90
suryaharahap <suryaharahap18@gmail.com>
2024-06-06 20:15:37 +0700

    feat: add routes in final project

---
+++ final-project/mygram/routes/route.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"golang-hactiv8-lab/final-project/mygram/controllers"
+	"golang-hactiv8-lab/final-project/mygram/handler"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+
+	if db == nil {
+		log.Fatalf("DB is nil")
+	}
+	userRepository := controllers.NewRepository(db)
+	if userRepository == nil {
+		log.Fatalf("userRepository is nil")
+	}
+
+	userService := controllers.NewService(userRepository)
+	if userService == nil {
+		log.Fatalf("userService is nil")
+	}
+
+	userHandler := handler.NewUserHandler(userService)
+	if userHandler == nil {
+		log.Fatalf("userHandler is nil")
+	}
+
+	r := gin.Default()
+	api := r.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
+	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
+
+	return r
+}
